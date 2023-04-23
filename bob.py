1a9b64205716e4d059b928b0fa200bb527b51302
orexXD <131597451+orexXD@users.noreply.github.com>
2023-04-23 16:45:05 +0300

    nobob

---
+++ bob.py
@@ -0,0 +1,29 @@
+bxhdfuv huehgudfhihudfbgjdf ig hfuagh udfughfu gldfjdfkbh dfuhbuf hudfhgudfuiudf 
+RS 
+ 
+  dh
+  d
+  h dg
+   h
+    d
+    g
+    dfh
+     t
+      hDF
+h 
+g
+hF 
+H
+zdhb
+ah
+
+sh
+ sfh
+  g fh
+  fgh 
+  
+  hsfhgh
+  sfhsf 
+  j sfg
+   sgfh gh gh 
+    gsj
